62a62d51353ba796704fa9e92b2ed9399fffd445
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2022-12-01 19:55:43 +0700

    Add files via upload

---
+++ users/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import CustomUser, Choice, Question
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+    readonly_fields = ['votes']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    readonly_fields = ['votes', 'voted_by', 'id']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(CustomUser)
+admin.site.register(Choice)
